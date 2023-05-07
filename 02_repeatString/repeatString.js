85c8eca89f611512aeae558dcb49a38471a6a33f
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-05-07 23:33:07 +0700

    meyelesaikan tugas 01

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let newString = "";
+  for (i = 1; i <= num; i++) {
+    newString += string;
+  }
 
+  return newString;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
