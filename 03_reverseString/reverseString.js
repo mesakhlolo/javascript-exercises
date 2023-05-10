d6270a2f60a49eb235f2ec373e275cd4e2002288
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-05-10 12:45:30 +0700

    menyelesaikan tugas 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+let reverseString = function (str) {
+  let newString = "";
 
+  if (str === "") {
+    return "";
+  }
+  for (i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+
+  return newString;
 };
+
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
