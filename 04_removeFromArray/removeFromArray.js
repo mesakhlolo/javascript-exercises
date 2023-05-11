a801c08456b94d59eb9cc3b67099bf074e3a6ac4
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-05-11 20:42:07 +0700

    menyelesaikan tugas 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(array, ...args) {
+  return array.filter((x) => !args.includes(x));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
