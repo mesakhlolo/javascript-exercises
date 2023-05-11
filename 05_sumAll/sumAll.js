2d716dcf9d9d6d12ba12cec5d077300fb17d2dc6
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-05-11 22:24:51 +0700

    menyelesaikan tugas 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+function sumAll(num1, num2) {
+  let sum = 0;
 
-};
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (!Number.isInteger(num1 && num2)) return "ERROR";
+
+  const minNum = Math.min(num1, num2);
+  const maxNum = Math.max(num1, num2);
+
+  for (let i = minNum; i <= maxNum; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
