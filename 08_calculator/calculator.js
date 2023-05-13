f106e4a3672d31f75e6f9185ce8da6576ebce715
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-05-13 17:38:43 +0700

    menyelesaikan tugas 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let newNum = 0;
+  arr.forEach((num) => {
+    newNum += num;
+  });
+
+  return newNum;
 };
 
-const multiply = function() {
+function multiply(arr) {
+  let newNum = 1;
+  arr.forEach((num) => {
+    newNum *= num;
+  });
 
+  return newNum;
+}
+
+const power = function (a, b) {
+  return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+function factorial(num) {
+  let result = 1;
+  for (let i = num; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
