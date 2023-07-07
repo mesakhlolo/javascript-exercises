4c59100cd00c3d02d857743869c3895cd56eddad
Mesakh Lolo <mesakhlolo12@gmail.com>
2023-07-07 18:34:28 +0700

    menyelesaikan tugas 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  if (n <= 2) {
+    return 1;
+  } else {
+    let sum = 0;
+    let a = 1;
+    let b = 1;
 
+    for (let i = 3; i <= n; i++) {
+      sum = a + b;
+      a = b;
+      b = sum;
+    }
+
+    return sum;
+  }
 };
 
 // Do not edit below this line
