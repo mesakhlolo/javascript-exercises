447698dc0678dc49027e66b1e51c20adbaa11862
Mesakh Lolo <mesakhlolo12@gmail.com>
2023-07-07 16:47:29 +0700

    menyelesaikan tugas 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  // Remove any non-alphanumeric characters and convert to lowercase
+  const cleanedWord = word.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  // Compare the cleaned word with its reversed version
+  const reversedWord = cleanedWord.split("").reverse().join("");
+  return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
