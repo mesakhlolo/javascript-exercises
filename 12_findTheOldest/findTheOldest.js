37921c91c8080e96ba6100b3a4f6c4a8e53f4708
Mesakh Lolo <mesakhlolo12@gmail.com>
2023-07-09 23:08:58 +0700

    menyelesaikan tugas 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentDate = new Date();
+  const oldestPerson = people.reduce((oldest, current) => {
+    const oldestAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : currentDate.getFullYear() - oldest.yearOfBirth;
+    const currentAge = current.yearOfDeath
+      ? current.yearOfDeath - current.yearOfBirth
+      : currentDate.getFullYear() - current.yearOfBirth;
+    return currentAge > oldestAge ? current : oldest;
+  });
 
+  return oldestPerson;
 };
 
 // Do not edit below this line
